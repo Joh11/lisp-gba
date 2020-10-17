791b7a830b6a6be2b6d5e6e9c6425d9b94f4eb78
Johan Félisaz <johan.felisaz@protonmail.com>
2020-10-17 19:52:14 +0200

    [x] displaying sprite

--- constants.h
+++ constants.h
@@ -3,6 +3,9 @@
 typedef unsigned char uint8;
 typedef unsigned short uint16;
 typedef unsigned int uint32;
+typedef signed char sint8;
+typedef signed short sint16;
+typedef signed int sint32;
 
 typedef struct { uint32 data[8]; } tile4;
 typedef struct { uint32 data[16]; } tile8;
@@ -15,8 +18,24 @@ typedef tile8 charblock8[256];
 
 typedef struct { uint16 data[256]; } palette;
 
-#define bg_palette_mem   ((palette*) 0x05000000)
-#define tile_palette_mem ((palette*) 0x05000200)
+#define bg_palette_mem   ((volatile palette*) 0x05000000)
+#define tile_palette_mem ((volatile palette*) 0x05000200)
 
 // Vsync
 #define REG_VCOUNT *(volatile uint16*)0x04000006
+
+// General settings for display
+#define REG_DISPCNT (volatile uint16*)0x04000000
+
+// Flags
+#define flag_video_mode0 0
+#define flag_enable_sprites (1 << 12)
+
+
+// Object attributes stuff
+typedef struct {
+    uint16 attr0;
+    uint16 attr1;
+    uint16 attr2;
+    sint16 fill;
+} __attribute__((aligned(4))) obj_attributes;
--- main.c
+++ main.c
@@ -4,25 +4,48 @@
 #define GREEN 0x03E0
 #define BLUE  0x7C00
 
+#define NULL 0
+
 static inline void vsync_wait()
 {
     while(REG_VCOUNT >= 160); // wait till VDraw
     while(REG_VCOUNT < 160);  // wait till VBlank
 }
 
+static inline uint16 rbg(int r, int g, int b)
+{
+    return (b << 10) | (g << 5) | r;
+}
+
 int main(void)
 {
-    // Write into the I/O registers, setting video display parameters.
-    volatile uint8 *ioram = (uint8 *)0x04000000;
-    ioram[0] = 0x03; // Use video mode 3 (in BG2, a 16bpp bitmap in VRAM)
-    ioram[1] = 0x04; // Enable BG2 (BG0 = 1, BG1 = 2, BG2 = 4, ...)
+    *REG_DISPCNT = flag_video_mode0 | flag_enable_sprites;
 
-    // Write pixel colours into VRAM
-    volatile uint16 *vram = (uint16 *)0x06000000;
-    vram[40*240 + 115] = RED; // X = 115, Y = 80, C = 000000000011111 = R
-    vram[80*240 + 120] = GREEN; // X = 120, Y = 80, C = 000001111100000 = G
-    vram[80*240 + 125] = BLUE; // X = 125, Y = 80, C = 111110000000000 = B
+    // sprite 0 of reg 5 will be totally white (the 2nd color of the palette)
+    tile4 white_sprite = {
+	{0x11111111, 0x22222222, 0x11111111, 0x11111111, 0x11111111, 0x11111111, 0x11111111, 0x11111111},
+    };
+    tile_mem[5][0] = white_sprite;
+    tile_mem[4][1] = white_sprite;
+    tile_mem[4][2] = white_sprite;
 
+    // set the palette
+    tile_palette_mem->data[0] = rbg(0, 31, 0); // transparency color is black
+    tile_palette_mem->data[1] = rbg(31, 31, 31); // first color is white
+    tile_palette_mem->data[2] = rbg(31, 0, 0); // third color is red
+
+    // set the object attributes
+    obj_attributes obj = {
+	50,  // y coord + other stuff
+	50,  // x coord + other stuff
+	512 | (0b11 << 0xA) | (0b00 << 0xC), // tile index + others stuff
+	NULL
+    };
+
+    ((volatile obj_attributes*)0x07000000)[0] = obj;
+
+    uint32 x = sizeof(tile4);
+    
     // Wait forever
     while(1)
     {
