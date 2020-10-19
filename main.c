0d30f4fc9d31365c55a8660239c09daa415a1383
Johan Félisaz <johan.felisaz@protonmail.com>
2020-10-19 08:30:25 +0200

    working cursor

--- main.c
+++ main.c
@@ -97,31 +97,18 @@ void fill_keyboard_tilemap(volatile screenblock sb)
 
 int main(void)
 {
-    const uint32 letter_lambda[2] = {0x10080804, 0x00442828};
-    const uint32 selector[2] = {0x810081db, 0xdb810081};
+    // const uint32 letter_lambda[2] = {0x10080804, 0x00442828};
+    // const uint32 selector[2] = {0x810081db, 0xdb810081};
     const uint32 big_selector[] = {0x10177, 0x10101, 0x8080ee, 0x808080, 0x1010100, 0x77010100, 0x80808000, 0xe7808000};
 
     REG_DISPCNT = flag_video_mode0 | flag_enable_bg0 | flag_enable_sprites | flag_1d_mapping;
 
-    // sprite 0 of reg 5 will be totally white (the 2nd color of the palette)
-    tile4 white_sprite = {
-	{0x11111111, 0x22222222, 0x11111111, 0x11111111, 0x11111111, 0x11111111, 0x11111111, 0x11111111},
-    };
-    tile_mem[5][0] = unpack_monochrome_tile4(letter_lambda, 0x1, 0x0);
-    unpack_monochrome_tiles(big_selector, &tile_mem[4][0], 4, 0x2, 0x0);
+    unpack_monochrome_tiles(big_selector, &tile_mem[4][1], 4, 0x2, 0x0);
         
     // set the palette
-    tile_palette_mem->data[0] = rbg(0, 0, 31); // transparency color is black
+    tile_palette_mem->data[0] = rbg(31, 0, 31); // transparency color is magenta
     tile_palette_mem->data[1] = rbg(31, 31, 31); // first color is white
     tile_palette_mem->data[2] = rbg(31, 0, 0); // third color is red
-
-    // set the lambda attributes
-    obj_attributes obj_lambda = {
-	160/2 + 16 + 4,  // y coord + other stuff
-	240/2 - 4,  // x coord + other stuff
-	512 | (0b11 << 0xA) | (0b00 << 0xC), // tile index + others stuff
-	0
-    };
 
     // set the selector attributes
     obj_attributes obj_selector = {
@@ -131,12 +118,14 @@ int main(void)
 	32 // x coord
 	| (0b01 << 0xe) // sprite size (16x16)
 	,
-	0 | (0b10 << 0xA) | (0b00 << 0xC), // tile index + others stuff
+	1 // tile index
+	| (0b10 << 0xA) // priority
+	| (0b00 << 0xC) // palette-bank
+	,
 	0
     };
 
-    // oam[1] = obj_lambda;
-    oam[2] = obj_selector;
+    oam[0] = obj_selector;
 
     const uint32 delay = 10;
     uint32 num_cycles_last_down = 0;
@@ -206,7 +195,7 @@ int main(void)
 	if(key_pressed_delay(ENUM_RIGHT))
 	    obj_selector.attr1 += 16;
 	
-	oam[2] = obj_selector;
+	oam[0] = obj_selector;
 	
 	update_key_state();
 
