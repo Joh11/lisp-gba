9f85d6440535c0ce007a0e9ba5e499adb7b35e13
Johan Félisaz <johan.felisaz@protonmail.com>
2020-10-19 08:59:48 +0200

    refactor bg

--- keyboard.c
+++ keyboard.c
@@ -9,6 +9,12 @@
 
 static const uint32 big_selector[] = {0x10177, 0x10101, 0x8080ee, 0x808080, 0x1010100, 0x77010100, 0x80808000, 0xe7808000};
 
+// tiles for the keycaps
+static const bitpacked_tile4 bg_tile = {0x00000000, 0x00000000};
+static const bitpacked_tile4 key_tl = {0x202fe00, 0x2020202};
+static const bitpacked_tile4 key_tr = {0x2020202, 0xfe0202};
+static const bitpacked_tile4 key_bl = {0x40407f00, 0x40404040};
+static const bitpacked_tile4 key_br = {0x40404040, 0x7f4040};
 
 // -----------------------------------------------------------------------------
 // Local variables
@@ -48,6 +54,22 @@ void init_keyboard()
     // -------------------------------------------------------------------------
     // Init backgrounds
     // -------------------------------------------------------------------------
+
+    // palette
+    bg_palette_mem->data[0] = rbg(0, 0, 0); // transparency color is black
+    bg_palette_mem->data[1] = rbg(31, 31, 31); // first color is white
+    bg_palette_mem->data[2] = rbg(15, 15, 15); // second color is gray
+
+    // tiles (use charblock 1)
+    tile_mem[1][0] = unpack_monochrome_tile4(bg_tile, 1, 0);
+    tile_mem[1][1] = unpack_monochrome_tile4(bg_tile, 1, 2);
+    tile_mem[1][2] = unpack_monochrome_tile4(key_tl, 2, 0);
+    tile_mem[1][3] = unpack_monochrome_tile4(key_tr, 2, 0);
+    tile_mem[1][4] = unpack_monochrome_tile4(key_bl, 2, 0);
+    tile_mem[1][5] = unpack_monochrome_tile4(key_br, 2, 0);
+
+    // tile map
+    fill_keyboard_tilemap(se_mem[0]);
 }
 
 void fill_keyboard_tilemap(volatile screenblock sb)
--- main.c
+++ main.c
@@ -33,39 +33,6 @@ int main(void)
 
     REG_BG0HOFS = 0;
     REG_BG0VOFS = 0;
-
-    // Use screenblock 0
-    fill_keyboard_tilemap(se_mem[0]);
-
-    // Fill the tiles
-    // Use the charblock 1
-    const bitpacked_tile4 black_tile = {0x00000000, 0x00000000};;
-    tile4 white_tile = {
-	{0x11111111, 0x11111111, 0x11111111, 0x11111111,
-	 0x11111111, 0x11111111, 0x11111111, 0x11111111},
-    };
-    tile4 gray_tile = {
-	{0x11111111, 0x12222221, 0x12222221, 0x12222221,
-	 0x12222221, 0x12222221, 0x12222221, 0x11111111},
-    };
-
-    // tiles for the keycaps
-    const bitpacked_tile4 key_tl = {0x202fe00, 0x2020202};
-    const bitpacked_tile4 key_tr = {0x2020202, 0xfe0202};
-    const bitpacked_tile4 key_bl = {0x40407f00, 0x40404040};
-    const bitpacked_tile4 key_br = {0x40404040, 0x7f4040};
-    
-    tile_mem[1][0] = unpack_monochrome_tile4(black_tile, 1, 0);
-    tile_mem[1][1] = gray_tile;
-    tile_mem[1][2] = unpack_monochrome_tile4(key_tl, 2, 0);
-    tile_mem[1][3] = unpack_monochrome_tile4(key_tr, 2, 0);
-    tile_mem[1][4] = unpack_monochrome_tile4(key_bl, 2, 0);
-    tile_mem[1][5] = unpack_monochrome_tile4(key_br, 2, 0);
-
-    // Set the palette
-    bg_palette_mem->data[0] = rbg(0, 0, 0); // transparency color is black
-    bg_palette_mem->data[1] = rbg(31, 31, 31); // first color is white
-    bg_palette_mem->data[2] = rbg(15, 15, 15); // second color is gray
     
     // Wait forever
     while(1)
