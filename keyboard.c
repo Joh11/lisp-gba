c702744b720f0b0dd16a494c158b0c3e6f02daab
Johan Félisaz <johan.felisaz@protonmail.com>
2020-10-20 12:22:09 +0200

    keycaps

--- keyboard.c
+++ keyboard.c
@@ -7,14 +7,20 @@
 // Constants
 // -----------------------------------------------------------------------------
 
-static const uint32 big_selector[] = {0x10177, 0x10101, 0x8080ee, 0x808080, 0x1010100, 0x77010100, 0x80808000, 0xe7808000};
+// static const uint32 big_selector[] = {0x10177, 0x10101, 0x8080ee, 0x808080, 0x1010100, 0x77010100, 0x80808000, 0xe7808000};
+static const uint32 big_selector[] = {0x10177, 0x10101, 0x8080ee, 0x808080, 0x1010100, 0x77010100, 0x808080, 0xee808000};
 
 // tiles for the keycaps
 static const bitpacked_tile4 bg_tile = {0x00000000, 0x00000000};
-static const bitpacked_tile4 key_tl = {0x202fe00, 0x2020202};
-static const bitpacked_tile4 key_tr = {0x2020202, 0xfe0202};
-static const bitpacked_tile4 key_bl = {0x40407f00, 0x40404040};
-static const bitpacked_tile4 key_br = {0x40404040, 0x7f4040};
+static const uint32 keycap[] = {0xfdfd01ff, 0xfdfdfdfd, 0xbfbf80ff, 0xbfbfbfbf, 0xfdfdfdfd, 0xff01fdfd, 0xbfbfbfbf, 0xff80bfbf};
+/* static const bitpacked_tile4 key_tl = {0x202fe00, 0x2020202}; */
+/* static const bitpacked_tile4 key_tr = {0x2020202, 0xfe0202}; */
+/* static const bitpacked_tile4 key_bl = {0x40407f00, 0x40404040}; */
+/* static const bitpacked_tile4 key_br = {0x40404040, 0x7f4040}; */
+
+// Font
+static const uint32 font[] = {0xf7f3f7ff, 0xe3f7f7f7, 0xdfdbe7ff, 0xc3fbf7ef, 0xdfdbe7ff, 0xe7dbdfe7, 0xdbd7cfff, 0xdfdfdfc3, 0xe3fbc3ff, 0xe3dfdfdf, 0xfbf7efff, 0xe7dbdbe3, 0xdfdfc3ff, 0xf7f7f7ef, 0xdbdbe7ff, 0xe7dbdbe7, 0xdbdbe7ff, 0xf7efdfc7, 0xcbdbe7ff, 0xe7dbdbd3, 0xffffffff, 0xffffffff, 0xdbdbe7ff, 0xdfdfc7db, 0xffffffff, 0xebd5d5dd, 0xe7ffffff, 0xc7fbc3db, 0xcbffffff, 0xfbfbfbf3, 0xfbe1fbff, 0xf7ebfbfb, 0xdbdbdbff, 0xe7dfdfc7, 0xdbffffff, 0xc7dbdbdb, 0xefffffff, 0xefefefff, 0xe7ffffff, 0xe7dbdbdb, 0xdbdbe7ff, 0xfbfbfbe3, 0xffffffff, 0xffffffff, 0xe7ffffff, 0xc7dbc7df, 0xc7ffffff, 0xe3dfe7fb, 0xdfdfffff, 0xc7dbdbc7, 0xfbdbe7ff, 0xfbfbe3fb, 0xdbc7ffff, 0xe7dfc7db, 0xfbfbffff, 0xdbdbdbe3, 0xefffefff, 0xf7ebefef, 0xdbdbfbff, 0xdbdbdbe3, 0xfbfbfbff, 0xf7fbfbfb, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff, 0xc3ffffff, 0xc3fbe7df, 0xdbffffff, 0xdbdbe7db, 0xe7ffffff, 0xe7dbfbdb, 0xbbffffff, 0xefd7bbbb, 0xfbfbfbff, 0xe3dbdbe3, 0xffffffff, 0xdbdbdbe3, 0xffffffff, 0xabababc3, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff};
+
 
 #define LINE_LEN 80
 static const uint8 char_lut[44] = "1234567890'qwertyuiop.asdfghjkl;,zxcvbnm(-+=";
@@ -72,6 +78,7 @@ void init_keyboard()
 
     // tiles
     unpack_monochrome_tiles(big_selector, &tile_mem[4][1], 4, 0x2, 0x0);
+    unpack_monochrome_tiles(font, &tile_mem[4][5], 44, 0x0, 0x1);
 
     // sprite
     obj_selector.attr0 = 96 // y coord
@@ -83,6 +90,19 @@ void init_keyboard()
 	| (0b00 << 0xC); // palette-bank
 
     oam[0] = obj_selector;
+    
+    // font object attributes
+    for(size_t i = 0; i < 44 ; ++i)
+    {
+	oam[1+i].attr0 = 100 + 16 * (i / 11) // y coord
+	    | (0b00 << 0xe); // sprite shape (8x8)
+	oam[1+i].attr1 = 36 + 16 * (i % 11) // x coord
+	    | (0b00 << 0xe); // sprite size (8x8)
+	oam[1+i].attr2 = 5+i // tile index
+	    | (0b10 << 0xA) // priority
+	    | (0b00 << 0xC); // palette-bank
+    }
+    
     
     // -------------------------------------------------------------------------
     // Init backgrounds
@@ -96,10 +116,11 @@ void init_keyboard()
     // tiles (use charblock 1)
     tile_mem[1][0] = unpack_monochrome_tile4(bg_tile, 1, 0);
     tile_mem[1][1] = unpack_monochrome_tile4(bg_tile, 1, 2);
-    tile_mem[1][2] = unpack_monochrome_tile4(key_tl, 2, 0);
-    tile_mem[1][3] = unpack_monochrome_tile4(key_tr, 2, 0);
-    tile_mem[1][4] = unpack_monochrome_tile4(key_bl, 2, 0);
-    tile_mem[1][5] = unpack_monochrome_tile4(key_br, 2, 0);
+    unpack_monochrome_tiles(keycap, &tile_mem[1][2], 4, 0x0, 0x2);
+    /* tile_mem[1][2] = unpack_monochrome_tile4(key_tl, 2, 0); */
+    /* tile_mem[1][3] = unpack_monochrome_tile4(key_tr, 2, 0); */
+    /* tile_mem[1][4] = unpack_monochrome_tile4(key_bl, 2, 0); */
+    /* tile_mem[1][5] = unpack_monochrome_tile4(key_br, 2, 0); */
 
     // tile map
     fill_keyboard_tilemap(se_mem[0]);
@@ -122,8 +143,8 @@ void fill_keyboard_tilemap(volatile screenblock sb)
 		continue;
 	    }
 	    sb[32 * row + col] = (row % 2) ?
-		3 + 2 * (col % 2)
-		: 2 + 2 * (col % 2);
+		4 + (col % 2)
+		: 2 + (col % 2);
 	}
     }
     for(size_t row = 20 ; row < 32 ; ++row)
@@ -177,7 +198,9 @@ void keyboard_handle_keypress(enum_key key)
 void toggle_keyboard_visibility()
 {
     keyboard_visiblep = !keyboard_visiblep;
-    obj_selector.attr0 ^= (0b1 << 9); // sprite
+    obj_selector.attr0 ^= (0b1 << 9); // selector
+    for(size_t i = 1 ; i < 45 ; ++i) // keys
+	oam[i].attr0 ^= (0b1 << 9);
     REG_DISPCNT ^= flag_enable_bg0;   // background
 }
 
